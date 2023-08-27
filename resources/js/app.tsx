5326f9063235bb75984c71163d28c1bdb60fa854
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2023-08-27 22:51:02 +0530

    initial commit

---
+++ resources/js/app.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import './bootstrap';
+
+import ReactDOM from 'react-dom/client';
+
+import Home from './components/Home';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <Home />
+);
---
+++ resources/js/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+    return <div className="container">
+        <div className="row my-5">
+            <div className="col-md-8 mx-auto">
+                <h1> Add React js to laravel 9 with vite 5</h1>
+            </div>
+        </div>
+    </div>;
+}
