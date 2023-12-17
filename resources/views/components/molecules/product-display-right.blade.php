a080e797dd6412007127104037684b4fa3c3596b
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2023-12-17 22:31:08 +0530

    update

--- resources/views/components/molecules/product-display-right.blade.php
+++ resources/views/components/molecules/product-display-right.blade.php
@@ -2,7 +2,7 @@
    
 
 
-    <x-molecules.product-display-price :varientId="{{@v}}" :productId="$product->id"  />
+    <x-molecules.product-display-price :varientId="1" :productId="$product->id"  />
 
     <x-molecules.product-display-input-group :varientId="1" :productId="$product->id"  />
    
